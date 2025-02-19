c5081e4fa73fe57250585da8fc75fb7a458ec92c
Abbas <Derabb0620@gmail.com>
2025-02-19 22:41:41 +0100

    lots of shit done today.

--- PortfolioBlazor/PortfolioBlazor/Models/TechsModel.cs
+++ PortfolioBlazor/PortfolioBlazor/Models/TechsModel.cs
@@ -1,21 +1,25 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace PortfolioBlazor.Models
 {
     public class TechsModel
     {
-        
-        
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Technology Name is required.")]
         [JsonPropertyName("technologies")]
         public string Technologies { get; set; }
 
         [JsonPropertyName("techExperience")]
+        [Required(ErrorMessage = "Technology Experience is required.")]
         public string TechExperience { get; set; }
 
         [JsonPropertyName("skillLevel")]
+        [Required(ErrorMessage = "Skill level is required.")]
         public double SkillLevel { get; set; }
+        [JsonPropertyName("image")]
+        public string? Image { get; set; }
     }
-
-
 }
-
--- PortfolioBlazor/PortfolioBlazor/wwwroot/Javascript/particles.js
+++ PortfolioBlazor/PortfolioBlazor/wwwroot/Javascript/particles.js
@@ -7,3 +7,13 @@
         console.error('particlesJS is not defined');
     }
 };
+
+window.loadClouds = () => {
+    if (typeof particlesJS !== 'undefined') {
+        particlesJS.load('cloud-js', '/cloud.json', function () {
+            console.log('particles.js loaded successfully');
+        });
+    } else {
+        console.error('particlesJS is not defined');
+    }
+};
